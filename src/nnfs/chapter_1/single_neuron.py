7088500184b8ca7f9f1db238a954c20457c1d835
Joe Keane <josephkeane996@gmail.com>
2025-11-14 17:05:46 +0000

    feat: started a python basics and added zips notebook

---
+++ src/nnfs/chapter_1/single_neuron.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib as mpl
+
+inputs = [1,2,3]
+weights = [.2, 0.8, -0.5]
+bias = 2
+output = (
+    inputs[0] * weights[0] +
+    inputs[1] * weights[1] +
+    inputs[2] * weights[2] +
+    bias
+)
+print(output)
