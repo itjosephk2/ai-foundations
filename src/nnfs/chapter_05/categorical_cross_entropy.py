a3e4ab484f5792a08306f5c99bbfe208cc2bd130
itjosephk2 <josephkeane996@gmail.com>
2026-04-06 18:42:47 +0100

    feat: chapter 5 updates

--- src/nnfs/chapter_05/categorical_cross_entropy.py
+++ src/nnfs/chapter_05/categorical_cross_entropy.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 # An example output from the output layer of the neural network
 softmax_output = [.7, .1, .2]
@@ -24,3 +25,48 @@ print(- math.log(0.2))
 print(- math.log(0.1))
 print(- math.log(0.05))
 print(- math.log(0.01))
+print('...')
+
+b = 5.2
+print(np.log(b))
+print(math.e ** 1.6486586255873816)
+print('...')
+
+# selecting the most confident answers - hard coded version
+softmax_outputs = [
+                    [0.7, 0.1, 0.2],
+                    [0.1, 0.5, 0.4],
+                    [0.02, 0.9, 0.08]
+                ]
+
+class_targets = [0, 1, 1]
+
+for targ_idx, distribution in zip(class_targets, softmax_outputs):
+    print(distribution[targ_idx])
+
+print('...')
+# Numpy version
+softmax_outputs = np.array([[0.7, 0.1, 0.2],
+                            [0.1, 0.5, 0.4],
+                            [0.02, 0.9, 0.08]])
+class_targets = [0, 1, 1]
+
+print(softmax_outputs[[0, 1, 2], class_targets])
+
+# using array length to iterate rather
+# than hardcoding the index numbers with softmax_outputs
+print(softmax_outputs[
+    range(len(softmax_outputs)), class_targets
+])
+
+# - log as per the math of the equation
+print(-np.log(softmax_outputs[
+    range(len(softmax_outputs)), class_targets
+]))
+
+# calculating average with numpy
+neg_log = -np.log(softmax_outputs[
+    range(len(softmax_outputs)), class_targets
+])
+average_loss = np.mean(neg_log)
+print(average_loss)
